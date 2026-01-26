868aa05ae4e0853d9b1d7cf4021f218d6e9bd77c
Cung <woaqcunq@gmail.com>
2026-01-26 13:46:45 +0700

    cấu hình .env

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
+require("dotenv").config();
 const express = require("express");
-// const mongoose = require("mongoose");
-// mongoose
-//   .connect("mongodb://127.0.0.1:27017/products-test")
-//   .then(() => console.log("Connected!"));
-// const Product = mongoose.model("Product", {
-//   title: String,
-//   price: Number,
-//   thumbnail: String,
-// });
-const route = require("./routes/client/index.route")
+const route = require("./routes/client/index.route");
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
