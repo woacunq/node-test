8eb9257f24725b33a6f35dfd852978156cb81641
Cung <woaqcunq@gmail.com>
2026-03-06 15:19:53 +0700

    trang products client

--- controllers/client/products.controller.js
+++ controllers/client/products.controller.js
@@ -1,5 +1,20 @@
-module.exports.index =  (req, res) => {
-    res.render("client/pages/products/index",{
-      pageTitle: "Danh sách sản phẩm"
-    });
-  }
+const Product = require('../../model/product.model');
+
+module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    status: 'active',
+    deleted: 'false',
+  });
+  const newProducts = products.map((item) => {
+    item.priceNew = (
+      (item.price * (100 - item.discountPercentage)) /
+      100
+    ).toFixed(0);
+    return item;
+  });
+
+  res.render('client/pages/products/index', {
+    pageTitle: 'Danh sách sản phẩm',
+    products: newProducts,
+  });
+};
