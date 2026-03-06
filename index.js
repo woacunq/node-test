4c831b39682508670bdf2c26d6fa53597b1d522a
Cung <woaqcunq@gmail.com>
2026-03-06 14:27:46 +0700

    Mongoose

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const express = require("express");
 const route = require("./routes/client/index.route");
 const app = express();
 const port = process.env.PORT;
+const database = require("./config/database.js");
 
+database.connect(); 
 app.set("views", "./views");
+
 app.set("view engine", "pug");
 
 app.use(express.static("public"));
