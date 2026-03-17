fdd5a20fec3a59721aba074cc9e17f70872130ba
Cung <woaqcunq@gmail.com>
2026-03-17 12:28:50 +0700

    Toi uu search

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,33 +1,9 @@
 // [GET] /admin/products
 const Product = require('../../model/product.model');
+const filterStatusHelper = require('../../helpers/filterStatus');
+const searchHelper = require('../../helpers/search');
 module.exports.index = async (req, res) => {
-  let filterStatus = [
-    {
-      name: 'Tất cả',
-      status: '',
-      class: '',
-    },
-    {
-      name: 'Hoạt động',
-      status: 'active',
-      class: '',
-    },
-    {
-      name: 'Dừng hoạt động',
-      status: 'inactive',
-      class: '',
-    },
-  ];
-
-  if (req.query.status) {
-    const index = filterStatus.findIndex(
-      (item) => item.status == req.query.status,
-    );
-    filterStatus[index].class = 'active';
-  } else {
-    const index = filterStatus.findIndex((item) => item.status == '');
-    filterStatus[index].class = 'active';
-  }
+  const filterStatus = filterStatusHelper(req.query);
 
   let find = {
     deleted: false,
@@ -36,24 +12,22 @@ module.exports.index = async (req, res) => {
   if (req.query.status) {
     find.status = req.query.status;
   }
+  // Search
+  const objectSearch = searchHelper(req.query);
 
-  let keyword = '';
-
-  if (req.query.keyword) {
-    keyword = req.query.keyword;
-    const regex = new RegExp(keyword, 'i');
-    find.title = regex;
+  if (objectSearch.regex) {
+    keyword = objectSearch.keyword;
+    find.title = objectSearch.regex;
   }
 
   const products = await Product.find(find);
 
   // console.log(products);
-  // Search
 
   res.render('admin/pages/products/index', {
     pageTitle: 'Trang danh sach san pham',
     products: products,
     filterStatus: filterStatus,
-    keyword: keyword,
+    keyword: objectSearch.keyword,
   });
 };
