71d6d666550117fbf219b7aaabda9eca074f0149
woacunq <woaqcunq@gmail.com>
2026-07-27 22:07:06 +0700

    Đăng nhập cơ bản chưa kèm middleware

---
+++ controllers/admin/auth.controller.js
@@ -0,0 +1,45 @@
+const Account = require("../../models/account.model")
+const systemConfig = require("../../config/system")
+const md5 = require("md5")
+
+
+// [GET] /admin/auth/login
+module.exports.login = (req, res) => {
+    res.render('admin/pages/auth/login', {
+        pageTitle: 'Trang đăng nhập',
+    });
+};
+
+// [POST] /admin/auth/login
+module.exports.loginPost = async (req, res) => {
+    const email = req.body.email
+    const password = req.body.password
+
+    const user = await Account.findOne({
+        email: email,
+        deleted: false
+    })
+
+    if (!user) {
+        req.flash("error", `${email} không tồn tại!`);
+        res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
+        return;
+    }
+
+    if (md5(password) != user.password) {
+        req.flash("error", `Sai mật khẩu!`);
+        res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
+        return;
+    }
+
+    if (user.status == 'inactive') {
+        req.flash("error", `Tài khoản đã bị khóa!`);
+        res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
+        return;
+    }
+
+    res.cookie("token", user.token)
+    res.redirect(`${systemConfig.prefixAdmin}/dashboard`);
+
+
+};
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -6,6 +6,8 @@ const categoryRoutes = require('./products-category.route');
 const uploadRoutes = require('./upload.route');
 const roleRoutes = require('./role.route');
 const accountRoutes = require('./account.route');
+const authRoutes = require('./auth.route');
+
 
 module.exports = (app) => {
   const PATH_ADMIN = systemConfig.prefixAdmin;
@@ -21,6 +23,8 @@ module.exports = (app) => {
   app.use(PATH_ADMIN + '/roles', roleRoutes);
 
   app.use(PATH_ADMIN + '/accounts', accountRoutes);
+
+  app.use(PATH_ADMIN + '/auth', authRoutes);
 
 
 };
